bc8ff05da7948025460d2e16f24ea5efc558bdb2
Samuel Pattuzzi <sam@pattuzzi.it>
2016-09-19 20:27:25 +0100

    Writing to the Console

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -5,7 +5,8 @@ public class ConsolePrinter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		print("Welcome to Go Home!");
+		print("A game were you need to find you way back.");
 	}
 	
 	// Update is called once per frame
