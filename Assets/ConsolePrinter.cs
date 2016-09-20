20c6d34f3f2b2291826ace63a3258eb8851d32e0
Samuel Pattuzzi <sam@pattuzzi.it>
2016-09-20 17:32:16 +0100

    Declaring and Assigning Variables

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -5,8 +5,12 @@ public class ConsolePrinter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		float location;
+		location = 5.0f; // Distance in meters
 		print("Welcome to Go Home!");
 		print("A game were you need to find you way back.");
+		print("Location:");
+		print(location);
 	}
 	
 	// Update is called once per frame
