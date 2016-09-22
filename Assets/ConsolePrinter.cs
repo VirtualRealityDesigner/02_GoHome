2bb57830f753594d727ff4b34dda8b05ea423a95
Samuel Pattuzzi <sam@pattuzzi.it>
2016-09-22 11:55:45 +0100

    Booleans and if statements

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -5,12 +5,24 @@ public class ConsolePrinter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float location = 3.0f; // Distance in meters
+		float location = 2.1f; // Distance in meters
 		float homeLocation = 2.1f;
 		float distance = homeLocation - location;
 		print("Welcome to Go Home!");
 		print("A game were you need to find you way back.");
-		print("Distance:" + distance);
+		print("Distance: " + distance);
+		if (location > homeLocation)
+		{
+			print("Go back to get home!");
+		}
+		if (location < homeLocation)
+		{
+			print("Go forward to get home!");
+		}
+		if (location == homeLocation)
+		{
+			print("I am at home!");
+		}
 	}
 	
 	// Update is called once per frame
