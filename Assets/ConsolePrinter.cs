c00b6d13eaacbca3f07bd228ffd33cc2248871fa
Samuel Pattuzzi <sam@pattuzzi.it>
2016-09-22 16:35:34 +0100

    Vectors, Classes and Objects

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -5,20 +5,13 @@ public class ConsolePrinter : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		float location = 2.1f; // Distance in meters
-		float homeLocation = 2.1f;
-		float distance = homeLocation - location;
+		Vector2 location = new Vector2(2.0f, 3.0f); // Distance in meters
+		Vector2 homeLocation = new Vector2(5.0f, 1.0f);
+		Vector2 pathToHome = homeLocation - location;
 		print("Welcome to Go Home!");
 		print("A game were you need to find you way back.");
-		print("Distance: " + distance);
-		if (location > homeLocation)
-		{
-			print("Go back to get home!");
-		}
-		if (location < homeLocation)
-		{
-			print("Go forward to get home!");
-		}
+		print("Path to home: " + pathToHome);
+		print("Distance to home: " + pathToHome.magnitude);
 		if (location == homeLocation)
 		{
 			print("I am at home!");
