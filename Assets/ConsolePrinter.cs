4c8c185bc78393c769fe295f86019558cb55bc94
Samuel Pattuzzi <sam@pattuzzi.it>
2016-09-26 12:17:22 +0100

    Don’t Repeat Yourself with Functions

--- Assets/ConsolePrinter.cs
+++ Assets/ConsolePrinter.cs
@@ -7,46 +7,33 @@ public class ConsolePrinter : MonoBehaviour {
 	public Vector2 homeLocation;
 
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		print("Welcome to Go Home!");
 		print("A game were you need to find you way back.");
+		Average(2f, 3f);
+
+		Average(5, 10);
+	}
+
+	void Average(float x, float y)
+	{
+		float avg1 = (x + y) / 2;
+		print("avg: " + avg1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(KeyCode.LeftArrow))
+		CheckForMovement(KeyCode.LeftArrow, new Vector2(-1, 0));
+		CheckForMovement(KeyCode.RightArrow, new Vector2(1, 0));
+		CheckForMovement(KeyCode.UpArrow, new Vector2(0, 1));
+		CheckForMovement(KeyCode.DownArrow, new Vector2(0, -1));
+	}
+
+	void CheckForMovement(KeyCode kc, Vector2 movementVector)
+	{
+		if (Input.GetKeyDown(kc))
 		{
-			location = location + new Vector2(-1, 0);
-			Vector2 pathToHome = homeLocation - location;
-			print("Distance to home: " + pathToHome.magnitude);
-			if (location == homeLocation)
-			{
-				print("I am at home!");
-			}
-		}
-		if (Input.GetKeyDown(KeyCode.RightArrow))
-		{
-			location = location + new Vector2(1, 0);
-			Vector2 pathToHome = homeLocation - location;
-			print("Distance to home: " + pathToHome.magnitude);
-			if (location == homeLocation)
-			{
-				print("I am at home!");
-			}
-		}
-		if (Input.GetKeyDown(KeyCode.UpArrow))
-		{
-			location = location + new Vector2(0, 1);
-			Vector2 pathToHome = homeLocation - location;
-			print("Distance to home: " + pathToHome.magnitude);
-			if (location == homeLocation)
-			{
-				print("I am at home!");
-			}
-		}
-		if (Input.GetKeyDown(KeyCode.DownArrow))
-		{
-			location = location + new Vector2(0, -1);
+			location = location + movementVector;
 			Vector2 pathToHome = homeLocation - location;
 			print("Distance to home: " + pathToHome.magnitude);
 			if (location == homeLocation)
